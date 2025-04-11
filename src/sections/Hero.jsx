23bc54d67585747beaafde4f1e7d752f6202be25
yedyng <ypy9kb@gmail.com>
2025-04-11 17:14:08 -0400

    Adding to the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,9 @@
 import { Element, Link as LinkScroll } from "react-scroll";
+import Button from "../components/Button.jsx";
 
 const Hero = () => {
   return (
-    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 border-amber-400">
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
       <Element name="hero">
         <div className="container">
           <div className="relative z-2 max-2512 max-lg:max-w-388">
@@ -17,8 +18,16 @@ const Hero = () => {
               learn, and astonishingly powerful.
             </p>
             <LinkScroll to="features" offset={-100} spy smooth>
-              TEXT
+              <Button icon="/images/zap.svg"> Try it now</Button>
             </LinkScroll>
+          </div>
+
+          <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
+            <img
+              src="/images/hero.png"
+              className="size-1230 max-lg:h-auto"
+              alt="hero"
+            />
           </div>
         </div>
       </Element>
